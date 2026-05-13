98f7e1e9219971d24af9ff83dbc98f876cfbd257
mingeon9411 <toube094@naver.com>
2026-05-13 18:56:57 +0900

    20260512(2)

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
 def landing(request):
-    return render(
-        request,
-        'single_pages/landing.html'
-    )
+    return render(request, 'single_pages/landing.html')
 
 def about_me(request):
-    return render(
-        request,
-        'single_pages/about_me.html'
-    )
+    return render(request, 'single_pages/about_me.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/blog/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'single_pages/signup.html', {'form': form})
